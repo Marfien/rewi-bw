24fc7ef3ec4105c4ca9eeeda16b881fb50bb418f
Marfien <marvin.sempa.haase@gmail.com>
2024-03-16 20:46:11 +0100

    Replaced logger with sout

--- anti-reduce-agent/src/main/java/dev/marfien/rewibw/agent/AntiReduceAgent.java
+++ anti-reduce-agent/src/main/java/dev/marfien/rewibw/agent/AntiReduceAgent.java
@@ -1,24 +1,18 @@
 package dev.marfien.rewibw.agent;
 
-import lombok.Getter;
-
 import java.lang.instrument.Instrumentation;
-import java.util.logging.Logger;
 
 public class AntiReduceAgent {
 
-    @Getter
-    private static final Logger logger = Logger.getLogger("AntiReduceAgent");
-
     public static void premain(String args, Instrumentation instrumentation) throws Exception {
-        logger.info("Loading AntiReduceAgent");
+        System.out.println("Loading AntiReduceAgent");
         try {
             instrumentation.addTransformer(new AntiReduceTransformer(), true);
         } catch (Exception e) {
-            logger.severe("Error loading AntiReduceAgent");
+            System.err.println("Error loading AntiReduceAgent");
             throw e;
         }
-        logger.info("AntiReduceAgent loaded");
+        System.out.println("AntiReduceAgent loaded");
     }
 
 }
